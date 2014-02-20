44964d9565c706afce9cc73cd855bac57de3dfb2
karthikramiah <karthik.r@gquotient.com>
2014-02-20 10:40:13 +0530

    added rdp group addition

--- user_mgmt.ps1
+++ user_mgmt.ps1
@@ -1,13 +1,13 @@
 #-----------------------------------------------------------------
 # Title: User Management Script
-# Created on : Jan 6th 2014
-# Version: 2.0
+# Created on : Jan 20th 2014
 #-----------------------------------------------------------------
 
 
 
 Function Createuser ($Name, $Password, $Group, $Description)
 {
+    
     $comp = [ADSI]"WinNT://$computer"
 	$user = $comp.Create("User", $Name)
 	$user.SetPassword($Password)
@@ -15,13 +15,19 @@ Function Createuser ($Name, $Password, $Group, $Description)
 	$user.Description= $Description
 	$user.SetInfo()
 	$user.UserFlags = 64 + 65536
-	$group = [ADSI]"WinNT://$computer/$Group,group"
+	if ($Group -ne 'Administrators') 
+    {
+		$rdu = [ADSI]("WinNT://$computer/Remote Desktop Users")
+		$rdu.add("WinNT://$computer/$Name") 
+    }
+    $group = [ADSI]"WinNT://$computer/$Group,group"
 	$group.add("WinNT://$computer/$Name")
-	#net localgroup $Group $Name /add
-	$user.SetInfo()
+    $user.SetInfo()
 }
 
-$servers = Get-Content server.txt
+#Update the servers list here
+
+$servers = Get-Content servers.txt
 
 $title = "Local User Account Management"
 
@@ -51,9 +57,12 @@ foreach ($computer in $servers)
 	
 	{
 	
-		if ($choice -eq 0) {
-	    
-	        Import-Csv users.csv | ForEach-Object {
+		if ($choice -eq 0) 
+
+		{
+			#Uppdate the userlist here
+	        
+			Import-Csv users.csv | ForEach-Object {
 		
 			$adsi = [ADSI]"WinNT://$computer"
 			
@@ -77,6 +86,9 @@ foreach ($computer in $servers)
 					
 					Createuser $_.Name $_.Password $_.Group $_.Description
 					
+					                          
+						
+					
 					Write-Host "User $usr added successfully to $computer" -BackgroundColor "Green" -ForegroundColor "Black"
 		
 				}
@@ -88,62 +100,34 @@ else
 
 	{
 	
+		$adsi = [ADSI]"WinNT://$computer"
+		
+		$localusers = $adsi.psbase.children | where {$_.psbase.schemaClassName -match "user"} | foreach {$_.name[0].tostring()}
+		
 		$users = Import-Csv users.csv | foreach {$_.Name.tostring()}
 		
-		$builtin = "Administrator", "cygserver_drakerwin", "Guest", "sshd";
-		
+		# Add all the Built-in accounts here
+
+		$builtin = "Administrator", "cyg_server", "Guest", "sshd", "LoggerNet", "SvcCWRSYNC", "Rack", "537551-admin", "555051-admin", "555050-admin";
+
 		foreach ($duser in @($localusers))
 
 		{
 
-			if ($users -notcontains $duser)
+			if (($users -notcontains $duser) -and ($builtin -notcontains $duser))
 
 			{
+				
+				write-host "User $duser doesn't exist in the Users list.Deleting user" -BackgroundColor "Red" -ForegroundColor "Black"
+				
+				$adsi.Delete("User", $duser)
+			
+				write-host "$duser deleted successfully" -BackgroundColor "Green" -ForegroundColor "Black"
+				
+			}	
+			
+		}
 	
-				if ($builtin -notcontains $duser)
-
-				{
-
-					write-host "User $duser doesn't exist in the list.Deleting user"
-					
-					$query = "Are you sure to delete the entered user?"
-		
-					$yes = new-Object System.Management.Automation.Host.ChoiceDescription "&Yes",`
-							"Accepting to delete the user account"
-				
-					$no = new-Object System.Management.Automation.Host.ChoiceDescription "&No",`
-							"Aborting the user deletion"
-				
-					$choices = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no)
-		
-					$answer = $host.ui.PromptForChoice($caption,$query,$choices,0)
-		
-					switch ($answer)
-		
-					{
-		
-						0 {"Deleting the user account....."}
-			
-						1 {"Aborting....."}
-		
-					}
-		
-					if ($answer -eq 0)
-
-					{
-		
-						$comp = [ADSI]"WinNT://$computer"
-		
-						$comp.Delete("User", $duser)
-			
-						"User $duser removed successfully!!!"
-		
-					}
-		
-					else
-		
-						{"Task Aborted!!!"}
-		
 	}
 
 }
